e7d1c8e17260be9641432da764602aa9b5661cf7
twopofc <pedrotwopofc@gmail.com>
2021-02-13 11:44:48 -0300

    First commit

---
+++ src/supporters.js
@@ -0,0 +1,17 @@
+const supporters = () => {
+    return `
+
+❌✞ ʟʊɨֆ ✞❌
+𝑁𝐾 𝑊𝐴𝑅 𝛩𝐹 𝑃𝜟𝐼𝑵𝜩𝑳
+
+
+
+Se vc quer se tornar um supporters,entre em contato com o meu dono,
+
+
+Aviso- pra se tornar supporters vc precisará rajar travas em pessoas q tentarem derrubar o bot.
+`
+
+}
+
+exports.supporters = supporters
---
+++ src/updates.js
@@ -0,0 +1,37 @@
+const updates = (prefix, pushname) => {
+ return `
+Novos comandos atualizados pelo meu dono 
+novos comandos :  
+
+${prefix}Ytmp4
+
+${prefix}Setppbot
+
+${prefix}Blocklist 
+
+${prefix}wame
+
+${prefix}image
+
+${prefix}loli(refeito funcionando)
+
+${prefix}grupo fechar
+
+${prefix}grupo abrir
+
+09/02/2021 Update:✞ঔ𝐓𝐫𝐚𝐬𝐡ф𝐃𝐤 𝒐͢𝒇𝒄ꪶ↷
+
+${prefix}pack+18
+
+${prefix}destrava
+
+
+
+
+digite ${prefix}help pra ver os comandos com mais detalhes
+ `
+
+
+}
+
+exports.updates = updates
